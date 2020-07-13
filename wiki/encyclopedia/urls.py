87bbad6bcf93fe8cb51dc588bcfe7dba9ce725ae
SylarK <syst.lucasamado@gmail.com>
2020-07-13 10:03:08 -0300

    The def entry is working very wel | Error: The def randomic isn't get the value.

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/<str:title>", views.entry, name="entry"),
+    path("/randomic", views.randomic, name="randomic")
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from . import util
+
+
+import random
+import markdown2
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def entry(request, title):
+    #check if is not empty
+    if util.get_entry(title):
+        cont = markdown2.markdown(util.get_entry(title))
+    else:
+        cont = ""
+    return render(request, "encyclopedia/entry.html", {
+
+        'content':cont,
+        'title':title
+
+    })
+
+def randomic(request):
+    title = random.choice(util.list_entries())
+    return HttpResponseRedirect(reverse("entry", kwargs={"title":title}))
+
 
