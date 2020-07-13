c63cadd360fad7505a3a9ca6100c19b425f7d323
LucasKaon <amado.github@gmail.com>
2020-07-13 13:24:38 -0300

    The def search is working. Was been used the function lower() for find a match

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.entry, name="entry"),
-    path("random", views.random, name="random")
+    path("random", views.random, name="random"),
+    path("search/<str:query>", views.search, name="search")
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render, redirect
 #from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django import forms
 
 from . import util
 
 from random import choice
 import markdown2
 
+#
 
+
+#
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
@@ -32,6 +36,39 @@ def random(request):
         "title":choice(util.list_entries())
 
     }))
+
+def search(request, query):
+
+    if request.method == "POST":
+        value = request.POST["search_query"]
+        list_names = util.list_entries()
+        if value in list_names:
+            cont = markdown2.markdown(util.get_entry(value))
+            return render(request, "encyclopedia/entry.html", {
+
+                'content':cont,
+                'title':value
+
+            })
+        else:
+            controlList = []
+            check = False
+            for item in list_names:
+                #check if the char of value is in list
+                if value.lower() in item.lower():
+                    controlList.append(item)
+                    check = True
+            
+            return render(request, "encyclopedia/search.html", {
+
+                "controlList":controlList,
+                "check":check
+
+            })
+
+        
+
+
 
 
 
